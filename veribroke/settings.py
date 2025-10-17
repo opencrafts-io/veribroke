66c29b81726366a7f81186f9f945fd7937120b5a
Baraka Mnjala Mbugua <i.am.mbuguaa34@gmail.com>
2025-10-17 13:12:33 +0300

    cleanup: Changed from sqlite to postgres because of buildpacks.
    Also this means that the stack is now the same across every service

--- veribroke/settings.py
+++ veribroke/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'veribroke.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME', default='veribroke'),
+        'USER': env('DB_USER', default='postgres'),
+        'PASSWORD': env('DB_PASSWORD', default=''),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='5432'),
     }
 }
 
