9b020ca3514e239dfb4463b7ad078fdcf5dcb8d0
Baraka Mnjala Mbugua <i.am.mbuguaa34@gmail.com>
2025-10-17 13:48:49 +0300

    Cleanup:Made DB password mandatory

--- veribroke/settings.py
+++ veribroke/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DB_NAME', default='veribroke'),
         'USER': env('DB_USER', default='postgres'),
-        'PASSWORD': env('DB_PASSWORD', default=''),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST', default='localhost'),
         'PORT': env('DB_PORT', default='5432'),
     }
