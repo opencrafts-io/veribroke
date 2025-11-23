a7c9909fbbe9e6aa3ddaf92dba0bee7b6168c41c
Your Name <rodney002mwanje@gmail.com>
2025-11-23 20:18:13 +0300

    fix: mpesa stk query failing

--- payments/utils.py
+++ payments/utils.py
@@ -18,10 +18,6 @@ def make_mpesa_stk(body):
         }        
 
         resp_status ,resp_data = mpesa_client.make_stk_push(mpesa_payload)
-        print(resp_status, resp_data)
-
-        print("Got here")
-
         if resp_status == 200:
             # await for 60 seconds
             num_retries = 0
@@ -29,11 +25,12 @@ def make_mpesa_stk(body):
             while True:
                 # TODO await all sleep and requests to the outside
                 time.sleep(1)
-                print("people")
                 # was successful query for status
                 trans_status, trans_resp = mpesa_client.query_transaction_status(
                     resp_data['CheckoutRequestID'],
                 )
+                if trans_resp['ResultDesc'] == 'The transaction is still under processing':
+                    continue
                 if trans_status == 200:
                     break
                 num_retries += 1
@@ -42,13 +39,13 @@ def make_mpesa_stk(body):
                     break
             # was successful querying
             if trans_status == 200:
+                
                 if trans_resp['ResultCode'] == '0':
                     return "Payment Received Successfully"
                 else:
                     # transaction was not successful
                     return "transaction was not successfully"
             else:
-                print("here is where it all happens")
                 return "Ooops Something Happened!Try Again Later"
         else:
             return resp_data['errorMessage']
